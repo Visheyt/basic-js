c50dd357ea28aa1a90a3f68ddf2127aa3e5cdf89
Alex <fornikaras7@gmail.com>
2023-12-15 15:37:25 +0500

    finish: basic what-season

--- src/what-season.js
+++ src/what-season.js
@@ -12,7 +12,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  return data[0]
+  let month = date.getMonth()
+  if (month>0 && month<2 || month === 11) {
+    return "winter"
+  }if (month>1 && month <5){
+    return "spring"
+  }if (month>4&& month < 8){
+    return "summer"
+  }
+  return "autumn"
 }
 
 module.exports = {
