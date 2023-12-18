a9c17eb0876eb0cfceda579e85c776a4455dbfa8
Alex <fornikaras7@gmail.com>
2023-12-18 11:05:41 +0500

    finish: sort-by-height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -12,10 +12,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
 function sortByHeight(arr) {
-  let newArr = arr.sort((a,b)=>{
-    if (a!== -1&& b!==-1){
-      return a-b
-    }})
+  let sortedArr = arr.filter(e=>e!==-1)
+  sortedArr.sort((a,b)=>a-b)
+  let newArr = []
+  for (let i = 0 ; i<arr.length;i++){
+    if (arr[i]!==-1){
+      newArr.push(0)
+    }else{newArr.push(-1)}
+  }
+  for (let j = 0 ;j<sortedArr.length;j++){
+    for (let q = 0 ;q<newArr.length;q++){
+      if (newArr[q]==0){
+        newArr[q] = sortedArr[j]
+        break
+      }
+    }
+  }
   return newArr
 }
 
