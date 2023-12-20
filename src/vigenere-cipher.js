e278a6dd3985033d7b97d8ae2897dd0a6d2358be
Alex <fornikaras7@gmail.com>
2023-12-20 23:49:50 +0500

    feat: add encrypt method in vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(command = true){
+    this.command = command
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message,key) {
+    if (!message ||!key){
+      throw new Error("Incorrect arguments!");
+    }
+    let str = message
+    let spaces = []
+    for (let i =0; i<str.length; i++){
+      if(str[i]==" "){
+        spaces.push(i)
+    }
+    }
+    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let finalStr = ""
+    let charsObj = new Object()
+    key = key.toUpperCase()
+    str = str.toUpperCase().replaceAll(" ","")
+    for (let i = 0 ;i<chars.length ; i++){
+      charsObj[chars[i]] = i
+   }
+    for (let j = 0; j<str.length;j++){
+      let x = (charsObj[str[j]]+charsObj[key[j%key.length]])%chars.length
+      if (!x && x!==0){
+        x = str[j]
+      }else {
+        x = chars[x]
+      }
+      finalStr +=x
+    }
+      for (let i = 0 ; i<spaces.length;i++){
+        for (let j = 0 ; j<finalStr.length;j++){
+          if (j == spaces[i]){
+            finalStr = finalStr.slice(0,j)+" "+finalStr.slice(j,finalStr.length)  
+          }
+        }
+      }
+      if (this.command == true ){return finalStr}
+
+  }
+  decrypt(encryptedMessage,key) {
+    if (!encryptedMessage ||!key){
+      throw new Error("Incorrect arguments!");
+    }
   }
 }
 
